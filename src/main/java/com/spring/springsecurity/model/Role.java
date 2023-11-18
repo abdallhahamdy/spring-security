8698f37b7c2cb0a27f4e8f425ceb92bb4da9263d
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-11-18 08:31:28 +0200

    hibernate and jpa with models

--- src/main/java/com/spring/springsecurity/model/Role.java
+++ src/main/java/com/spring/springsecurity/model/Role.java
@@ -2,11 +2,19 @@ package com.spring.springsecurity.model;
 
 import java.util.Set;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "role_name")
     private String roleName;
 
+    @ManyToMany(mappedBy = "roles")
     private Set<User> users;
 }

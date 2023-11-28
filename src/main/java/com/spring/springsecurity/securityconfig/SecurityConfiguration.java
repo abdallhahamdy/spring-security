4bb89d5e58cf9f497c0ad03e618d57de17be19db
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-11-28 07:28:35 +0200

    editing SecurityConfiguration

--- src/main/java/com/spring/springsecurity/securityconfig/SecurityConfiguration.java
+++ src/main/java/com/spring/springsecurity/securityconfig/SecurityConfiguration.java
@@ -1,55 +1,54 @@
 package com.spring.springsecurity.securityconfig;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.spring.springsecurity.dao.UserRepository;
+import com.spring.springsecurity.securityconfig.jwt.JwtAuthenticationFilter;
+import com.spring.springsecurity.securityconfig.jwt.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private UserPrincipalDetailsService userPrincipalDetailsService;
+    private UserRepository userRepository;
 
-    @Autowired
-    public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService) {
+    public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService, UserRepository userRepository) {
         this.userPrincipalDetailsService = userPrincipalDetailsService;
+        this.userRepository = userRepository;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
-
     }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(),this.userRepository))
                 .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/login").permitAll()
                 .antMatchers("/api/main").permitAll()
                 .antMatchers("/api/profile").authenticated()
                 .antMatchers("/api/admin/**").hasRole("ADMIN")
                 .antMatchers("/api/manage").hasAnyRole("ADMIN","MANGER")
-                .antMatchers("/api/basic/mybasic").hasAuthority("ACCESS_API")
-                .antMatchers("/api/basic/allbasic").hasAuthority("ACCESS_BASIC")
-                //.anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginProcessingUrl("/signin")
-                .loginPage("/api/login")
-                .usernameParameter("user")
-                .passwordParameter("pass")
-                .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/api/main");
+                .antMatchers("/api/basic/mybasic").hasAuthority("ACCESS_BASIC1")
+                .antMatchers("/api/basic/allbasic").hasAuthority("ACCESS_BASIC2")
+                .anyRequest().authenticated();
     }
-
     @Bean
     DaoAuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
@@ -57,9 +56,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         daoAuthenticationProvider.setUserDetailsService(userPrincipalDetailsService);
         return daoAuthenticationProvider;
     }
-
     @Bean
-    PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 }
